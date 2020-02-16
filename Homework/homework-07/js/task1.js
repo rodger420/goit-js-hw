4d1ef05810667bcd8ff90153649ff98f29eb6d3f
Anatoliy <Blizzabum@Gmail.com>
2020-02-16 15:57:26 +0200

    commit

--- Homework/homework-07/js/task1.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const liItems = document.querySelectorAll('.item');
-console.log(liItems.length);
-const item = document.querySelector('.item');
-for (let i = 0; i < liItems.length; i += 1) {
-  console.log(
-    `Категория: ${liItems[i].firstElementChild.textContent},
-     Количество элементов: ${liItems[i].lastElementChild.children.length}`);
-}
--- Homework/homework-07/js/task5.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const input = document.querySelector('#name-input');
-const outputSpan = document.querySelector('#name-output');
-
-const inputText = () => {
-  outputSpan.textContent = event.currentTarget.value;
-  if (input.value === "") outputSpan.textContent = 'Незнакомец';
-}
-input.addEventListener('input', inputText);
